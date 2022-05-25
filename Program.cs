9cdc9ef07861db389ec4934d5b624455de7ee5cb
Роман <ram23@mail.ru>
2022-05-25 16:29:18 +0300

    Add Method Addition

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ namespace ConsoleITVDN
 {
     internal class Program
     {
+        static int Addition(int num1, int num2) 
+        { 
+            return num1 + num2;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("My first commit");
